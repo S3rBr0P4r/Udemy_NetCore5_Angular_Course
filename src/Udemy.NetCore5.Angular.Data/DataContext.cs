074fa6bc756b91e20d4c92e21b018825daf9e3b5
S3rBr0P4r <5878174+S3rBr0P4r@users.noreply.github.com>
2020-12-13 16:18:08 +0100

    Solution adjusted to have initial database with dummy data

---
+++ src/Udemy.NetCore5.Angular.Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Udemy.NetCore5.Angular.Data.Entities;
+
+namespace Udemy.NetCore5.Angular.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ src/Udemy.NetCore5.Angular.Data/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Udemy.NetCore5.Angular.Data.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
