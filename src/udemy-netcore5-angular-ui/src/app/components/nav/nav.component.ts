6f0edffe14e517d9746832e7eb318e3d844612ab
S3rBr0P4r <5878174+S3rBr0P4r@users.noreply.github.com>
2021-01-02 16:02:45 +0100

    Routing in Angular project implemented, as well as empty components added for routing

--- src/udemy-netcore5-angular-ui/src/app/components/nav/nav.component.ts
+++ src/udemy-netcore5-angular-ui/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { error } from 'protractor';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
@@ -12,14 +13,14 @@ import { AccountService } from 'src/app/services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     },
     error => {
       console.log(error)
@@ -28,5 +29,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
