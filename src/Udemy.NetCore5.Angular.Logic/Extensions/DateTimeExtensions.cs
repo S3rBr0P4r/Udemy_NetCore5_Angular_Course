857baa9a2878d47a8249e1d0a678dac366605318
S3rBr0P4r <5878174+S3rBr0P4r@users.noreply.github.com>
2021-01-03 15:31:20 +0100

    Datetime extension to calculate the age added

---
+++ src/Udemy.NetCore5.Angular.Logic/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Udemy.NetCore5.Angular.Logic.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+            if (dateOfBirth.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
