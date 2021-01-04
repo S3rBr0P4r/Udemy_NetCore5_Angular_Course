7c97c543a10f95cc7efbf19080cea4a39f590d82
S3rBr0P4r <5878174+S3rBr0P4r@users.noreply.github.com>
2021-01-04 18:47:07 +0100

    - Environment files adjusted to include baseUrl - First version of members service added

---
+++ src/udemy-netcore5-angular-ui/src/app/services/members.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getMembers() {
+    return this.httpClient.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string) {
+    return this.httpClient.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
