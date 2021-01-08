1c9409173b1f72b6104710827bb0f01b517f5973
S3rBr0P4r <5878174+S3rBr0P4r@users.noreply.github.com>
2021-01-08 17:31:56 +0100

    First version of Messages controller added

---
+++ src/Udemy.NetCore5.Angular.Api/Controllers/MessagesController.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Udemy.NetCore5.Angular.Data.Entities;
+using Udemy.NetCore5.Angular.Logic.DTOs;
+using Udemy.NetCore5.Angular.Logic.Extensions;
+using Udemy.NetCore5.Angular.Logic.Helpers;
+using Udemy.NetCore5.Angular.Logic.Interfaces;
+
+namespace Udemy.NetCore5.Angular.Api.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IAppUserRepository _userRepository;
+        private readonly IMessagesRepository _messagesRepository;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IAppUserRepository userRepository, IMessagesRepository messagesRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _messagesRepository = messagesRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppUserMessagesResponse>> CreateMessage([FromBody] AppUserMessagesRequest request)
+        {
+            var userName = User.GetUserName();
+
+            if (userName == request.RecipientUserName.ToLowerInvariant())
+            {
+                return BadRequest("You cannot send messages to yourself");
+            }
+
+            var sender = await GetUser(userName).ConfigureAwait(false);
+            var recipient = await GetUser(request.RecipientUserName).ConfigureAwait(false);
+
+            if (recipient == null)
+            {
+                return NotFound();
+            }
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUserName = sender.UserName,
+                RecipientUserName = recipient.UserName,
+                Content = request.Content
+            };
+
+            _messagesRepository.AddMessage(message);
+
+            if (await _messagesRepository.SaveAllAsync())
+            {
+                return Ok(_mapper.Map<AppUserMessagesResponse>(message));
+            }
+
+            return BadRequest("Failed to send message");
+        }
+
+        private async Task<AppUser> GetUser(string userName)
+        {
+            var userResponse = await _userRepository.GetUserByUserNameAsync(userName).ConfigureAwait(false);
+            return await _userRepository.GetUserByIdAsync(userResponse.Id).ConfigureAwait(false);
+        }
+    }
+}
